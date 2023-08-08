fe7850135828f779e90d1144c236ea537af161bc
Viktor <vinill2@ukr.net>
2023-08-08 23:15:13 +0300

    code refactoring

--- SimpleChat/SimpleChat/UserChatsModule/UserChatsViewController/UserCollectionViewCell/UserCollectionViewCell.swift
+++ SimpleChat/SimpleChat/UserChatsModule/UserChatsViewController/UserCollectionViewCell/UserCollectionViewCell.swift
@@ -10,6 +10,7 @@ import SDWebImage
 
 class UserCollectionViewCell: UICollectionViewCell, Registrateble {
 
+    @IBOutlet weak var containerStackView: UIStackView!
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet private weak var firstNameLabel: UILabel!
     
@@ -25,7 +26,12 @@ class UserCollectionViewCell: UICollectionViewCell, Registrateble {
     
     func fillWith(_ user: User) {
         let placeholderImage = UIImage(systemName: Constants.Strings.avatarPlaceholder)?.withTintColor(.black)
-        avatarImageView.sd_setImage(with: URL(string: user.imageUrl), placeholderImage: placeholderImage)
+        avatarImageView.sd_setImage(with: URL(string: user.imageUrl), placeholderImage: placeholderImage) { _, _, _, _ in
+            self.containerStackView.setNeedsLayout()
+            self.avatarImageView.setNeedsLayout()
+        }
         firstNameLabel.text = user.firstName
+        self.containerStackView.setNeedsLayout()
+        self.avatarImageView.setNeedsLayout()
     }
 }
